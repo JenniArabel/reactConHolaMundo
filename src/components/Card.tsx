327f452c098aaec88b302838c4e41d056d8c6fe6
Jennifer Arabel <jennifer.arabel@gmail.com>
2024-12-04 13:25:13 -0300

    Add a Fragment

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,32 @@
 function Card() {
-    
   return (
-    <div className="card" style={{
+    <div
+      className="card"
+      style={{
         width: "350px",
-    }}>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text"> Some quick example text to build on the card title and make up the bulk of the card's content. </p>
-        <a href="#" className="btn btn-primary"> Go somewhere </a>
+      }}
+    >
+      <div className="card-body"> 
+        <CardBody />
       </div>
     </div>
+  );
+}
+
+export function CardBody() {
+  return (
+    <> 
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        {" "}
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.{" "}
+      </p>
+      <a href="#" className="btn btn-primary">
+        {" "}
+        Go somewhere{" "}
+      </a>
+    </>
   );
 }
 
